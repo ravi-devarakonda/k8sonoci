5a0eba1e835848020ac70c10dc69f08ed2acb1ac
Oracle Public Cloud User <opc@solartis-workshop.subnet.vcn.oraclevcn.com>
2020-09-24 09:18:13 +0000

    added ingress backend

---
+++ ingress/deploy-cheese.sh
@@ -0,0 +1,9 @@
+#create deployments
+kubectl create deployment cheddar --image=errm/cheese:cheddar
+kubectl create deployment stilton --image=errm/cheese:stilton
+kubectl create deployment wensleydale --image=errm/cheese:wensleydale
+
+#create corresponding services
+kubectl expose deployment cheddar --port=80
+kubectl expose deployment stilton --port=80
+kubectl expose deployment wensleydale --port=80
